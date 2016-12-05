73c546d5682b54ee792c45d2ca219479b560796a
Kalen Peterson <kpeterson@forsythe.com>
2016-12-05 08:50:23 -0600

    fix pe_repo

--- manifests/puppet/pe_masterofmasters.pp
+++ manifests/puppet/pe_masterofmasters.pp
@@ -18,7 +18,7 @@ class roles::puppet::pe_masterofmasters {
   include ::puppet_enterprise::profile::master
   include ::puppet_enterprise::profile::master::mcollective
   include ::puppet_enterprise::profile::mcollective::peadmin
-  #include ::pe_repo
+  include ::pe_repo
   include ::pe_repo::platform::el_7_x86_64
 
   #include profiles::puppet::pe_mcollective
