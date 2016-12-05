a6a65d6a0a75cc2f457319902edadd6296b3ceba
Kalen Peterson <kpeterson@forsythe.com>
2016-12-05 12:45:05 -0600

    ensure profiles in masterofmaster are top-scoped

--- manifests/puppet/pe_masterofmasters.pp
+++ manifests/puppet/pe_masterofmasters.pp
@@ -2,31 +2,31 @@
 #
 class roles::puppet::pe_masterofmasters {
   #include profiles::puppet::pe_infrastructure
-  include puppet_enterprise
+  include ::puppet_enterprise
 
   #include profiles::puppet::pe_agent
-  include puppet_enterprise::profile::agent
+  include ::puppet_enterprise::profile::agent
 
   #include profiles::puppet::pe_certificate_authority
-  include puppet_enterprise::profile::certificate_authority
+  include ::puppet_enterprise::profile::certificate_authority
 
   #include profiles::puppet::pe_console
-  include puppet_enterprise::profile::console
-  include puppet_enterprise::license
+  include ::puppet_enterprise::profile::console
+  include ::puppet_enterprise::license
 
   #include profiles::puppet::pe_master
-  include puppet_enterprise::profile::master
-  include puppet_enterprise::profile::master::mcollective
-  include puppet_enterprise::profile::mcollective::peadmin
-  include pe_repo
-  include pe_repo::platform::el_7_x86_64
+  include ::puppet_enterprise::profile::master
+  include ::puppet_enterprise::profile::master::mcollective
+  include ::puppet_enterprise::profile::mcollective::peadmin
+  include ::pe_repo
+  include ::pe_repo::platform::el_7_x86_64
 
   #include profiles::puppet::pe_mcollective
-  include puppet_enterprise::profile::mcollective::agent
+  include ::puppet_enterprise::profile::mcollective::agent
 
   #include profiles::puppet::pe_orchestrator
-  include puppet_enterprise::profile::orchestrator
+  include ::puppet_enterprise::profile::orchestrator
 
   #include profiles::puppet::pe_puppetdb
-  include puppet_enterprise::profile::puppetdb
+  include ::puppet_enterprise::profile::puppetdb
 }
